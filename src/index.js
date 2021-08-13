02e8c0be93c967f55ddf20fef305027c0b6e5620
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-13 00:41:35 -0300

    feat: added css reset and basic global styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import "./assets/styles/reset.css";
+import "./assets/styles/style.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
