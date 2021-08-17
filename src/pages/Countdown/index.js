6d08cb549f3221a8ee40f800425c557a40d70538
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-17 00:32:12 -0300

    feat: countdown page redirects to enroll when timer reaches 0

--- src/pages/Countdown/index.js
+++ src/pages/Countdown/index.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import Timer from "./Timer";
 import Page from "../../components/Page";
@@ -7,6 +8,11 @@ import EventInfoContext from "../../contexts/EventInfoContext";
 
 export default function Countdown() {
   const { eventInfo, loadingEventInfo } = useContext(EventInfoContext);
+  const history = useHistory();
+
+  function onZero() {
+    history.push("/enroll");
+  }
 
   if (loadingEventInfo) {
     return "Loading...";
@@ -15,7 +21,7 @@ export default function Countdown() {
   return (
     <Page background={eventInfo.backgroundImage}>
       <div>Faltam</div>
-      <Timer time={eventInfo.startDate} />
+      <Timer time={eventInfo.startDate} onZero={onZero} />
       <div>Para as inscrições</div>
     </Page>
   );
