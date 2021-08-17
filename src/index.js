1c838de47585fafcc39c0c625ffbda74d393841d
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-16 23:53:44 -0300

    fix: removed react strict mode because toast was being called twice

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import "react-toastify/dist/ReactToastify.css";
+
 import "./assets/styles/reset.css";
 import "./assets/styles/style.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
