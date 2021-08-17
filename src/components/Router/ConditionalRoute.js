a827266ca97975f69e27bc02527ba002f09faeef
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-16 23:35:07 -0300

    fix: conditional route component calls a function instead of receiving array

--- src/components/Router/ConditionalRoute.js
+++ src/components/Router/ConditionalRoute.js
@@ -1,7 +1,9 @@
 import { Route, Redirect } from "react-router-dom";
 
-export default function ConditionalRoute({ check=[], ...props  }) {
-  for (const condition of check) {
+export default function ConditionalRoute({ check=() => [], ...props  }) {
+  const validations = check();
+
+  for (const condition of validations) {
     if (!condition.check()) {
       return (
         <Redirect to={condition.to} />
