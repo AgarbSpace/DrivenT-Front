5dd44ff7848013a782b47453dac6708085a6d0d0
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-17 16:47:56 -0300

    feat: container element for all pages

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default styled.div`
+  height: 100vh;
+  max-height: 800px;
+
+  width: 100%;
+  max-width: 1200px;
+
+  border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 4px 4px 10px 5px rgba(0, 0, 0, .2);
+
+  display: flex;
+`;
