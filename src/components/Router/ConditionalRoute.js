97a2c937845ae2601b69dbed405f833f21867fec
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-16 23:53:27 -0300

    feat: added toast message to conditional route when redirect happens

--- src/components/Router/ConditionalRoute.js
+++ src/components/Router/ConditionalRoute.js
@@ -1,10 +1,15 @@
 import { Route, Redirect } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function ConditionalRoute({ check=() => [], ...props  }) {
   const validations = check();
 
   for (const condition of validations) {
     if (!condition.check()) {
+      if (condition.message) {
+        toast(condition.message);
+      }
+
       return (
         <Redirect to={condition.to} />
       );
