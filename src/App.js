04fd6246639b116b9b9199ee5b9a416056e58b00
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-16 23:42:23 -0300

    feat: using conditional route component to stop user from accessing countdown route or enroll route when they are not supposed to

--- src/App.js
+++ src/App.js
@@ -3,26 +3,46 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { useContext } from "react";
+import dayjs from "dayjs";
+
+import ConditionalRoute from "./components/Router/ConditionalRoute";
 
 import Countdown from "./pages/Countdown";
 import Enroll from "./pages/Enroll";
 
-import { EventInfoProvider } from "./contexts/EventInfoContext";
+import EventInfoContext, { EventInfoProvider } from "./contexts/EventInfoContext";
 
 export default function App() {
   return (
     <EventInfoProvider>
       <Router>
         <Switch>
-          <Route path="/" exact>
+          <ConditionalRoute check={ensureCountdownOngoing} path="/" exact>
             <Countdown />
-          </Route>
+          </ConditionalRoute>
 
-          <Route path="/enroll" exact>
+          <ConditionalRoute check={ensureCountdownOver} path="/enroll" exact>
             <Enroll />
-          </Route>
+          </ConditionalRoute>
         </Switch>
       </Router>
     </EventInfoProvider>
   );
 }
+
+function ensureCountdownOngoing() {
+  const { eventInfo } = useContext(EventInfoContext);
+
+  return [
+    { to: "/enroll", check: () => dayjs().isBefore(dayjs(eventInfo.startDate)) }
+  ];
+}
+
+function ensureCountdownOver() {
+  const { eventInfo } = useContext(EventInfoContext);
+
+  return [
+    { to: "/", check: () => dayjs().isAfter(dayjs(eventInfo.startDate)) }
+  ];
+} 
