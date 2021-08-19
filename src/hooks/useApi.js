dd8e2a366f7a818a7fe084c0fdc44705154eb867
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-19 13:15:26 -0300

    feat: added auth api service to useApi hook

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,9 +1,11 @@
 import EventApi from "../services/event";
 import EnrollmentApi from "../services/enrollment";
+import AuthApi from "../services/auth";
 
 export default function useApi() {
   return {
     event: new EventApi(),
-    enrollment: new EnrollmentApi()
+    enrollment: new EnrollmentApi(),
+    auth: new AuthApi()
   };
 }
