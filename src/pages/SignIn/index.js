282581c3422318e37d8b3dd1f33f1147d17edd78
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-19 13:12:31 -0300

    feat: login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,3 +1,43 @@
+import { useState, useContext } from "react";
+
+import AuthLayout from "../../layouts/Auth";
+
+import Input from "../../components/Form/Input";
+import Button from "../../components/Form/Button"
+import Link from "../../components/Link";
+import { Column, Title, Label } from "../../components/Auth";
+
+import EventInfoContext from "../../contexts/EventInfoContext";
+
 export default function SignIn() {
-  return "Sign In!";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loadingSignIn, setLoadingSignIn] = useState(false);
+
+  const { eventInfo } = useContext(EventInfoContext);
+  
+  function submit(event) {
+    event.preventDefault();
+    setLoadingSignIn(true);
+  } 
+
+  return (
+    <AuthLayout background={eventInfo.backgroundImage}>
+      <Column>
+        <img src={eventInfo.logoImage} alt="Event Logo" />
+        <Title>{eventInfo.eventTitle}</Title>
+      </Column>
+      <Column>
+        <Label>Inscrição</Label>
+        <form onSubmit={submit}>
+          <Input label="E-mail" type="text" fullWidth value={email} onChange={e => setEmail(e.target.value)} />
+          <Input label="Senha" type="password" fullWidth value={password} onChange={e => setPassword(e.target.value)} />
+          <Button type="submit" color="primary" fullWidth disabled={loadingSignIn}>Entrar</Button>
+        </form>
+      </Column>
+      <Column>
+        <Link to="/enroll">Não possui login? Inscreva-se</Link>
+      </Column>
+    </AuthLayout>
+  );
 }
