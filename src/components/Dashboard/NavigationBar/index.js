486432936837d42142de69458bad97dc8b595afe
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-08-24 00:48:49 -0300

    feat: created dashboard sidebar

---
+++ src/components/Dashboard/NavigationBar/index.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+import {
+  FaFileContract,
+  FaMoneyBill,
+  FaBed,
+  FaCalendarWeek,
+  FaCertificate
+} from "react-icons/fa";
+
+import NavigationButton from "./NavigationButton";
+
+export default function NavigationBar() {
+  return (
+    <Container>
+      <NavigationButton>
+        <FaFileContract />
+        <span>Inscrição</span>
+      </NavigationButton>
+
+      <NavigationButton>
+        <FaMoneyBill />
+        <span>Pagamento</span>
+      </NavigationButton>
+
+      <NavigationButton>
+        <FaBed />
+        <span>Hotel</span>
+      </NavigationButton>
+      
+      <NavigationButton>
+        <FaCalendarWeek />
+        <span>Atividades</span>
+      </NavigationButton>
+
+      <NavigationButton>
+        <FaCertificate />
+        <span>Certificado</span>
+      </NavigationButton>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #A7A7A7;
+  width: 100px;
+  flex-shrink: 0;
+  justify-content: space-between;
+`;
