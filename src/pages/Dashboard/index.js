bed9db432e950df09cd5ff45c58c10a82668152e
Guga <contato.gustavobarbosa@outlook.com>
2021-08-24 15:26:34 -0300

    feat: certificate main structure with download functionality

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,3 +1,15 @@
+import Certificate from "../../components/Certificate";
+
 export default function Dashboard() {
-  return "Dashboard!";
+  return (
+    <Certificate
+      color={"#f77dae"}
+      attendeeName={"Gustavo Barbosa Santos"}
+      activities={["Palestra 1", "Palestra 2", "Palestra 3"]}
+      startEventDate={"2021-08-20T20:30:00.000Z"}
+      endEventDate={"2021-08-25T20:30:00.000Z"}
+      workload={10}
+      credentialNumber={"abcd-efgh-ijkl-mnop"}
+    />
+  );
 }
